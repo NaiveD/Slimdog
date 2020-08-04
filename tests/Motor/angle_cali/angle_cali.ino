dde2432938d3c2cc26fcd6e37ba578accfc67e1c
AdvisorChuanChuan <licha@shanghaitech.edu.cn>
2020-08-04 16:05:24 +0800

    adjust servo5 in45 sample

--- tests/Motor/angle_cali/angle_cali.ino
+++ tests/Motor/angle_cali/angle_cali.ino
@@ -63,7 +63,7 @@ void setup() {
 //  pwm.setPWM(2, 0, angletoPWM(angle, 2));
 //
 //  pwm.setPWM(4, 0, 580); // 220 top , 400 horizontal (90), 310 (60), 580 bottom     220 - 400 - 580(tallest)
-//  pwm.setPWM(5, 0, 330); // 330 vertical to the ground, 400 out30, 190 in45           110 - 330 - 470(outermost / rightmost)
+  pwm.setPWM(5, 0, 210); // 330 vertical to the ground, 400 out30, 190 in45           110 - 330 - 470(outermost / rightmost)
 //  pwm.setPWM(6, 0, 345); // 345 verticle to the ground, 220 (45 to the back), 480 (45 to the front)   110 - 345 - ~590 (frontmost)
 //
 //  pwm.setPWM(8, 0, 250); // 380 horizontal (90), ~140 tallest, ~550 shortest (~45 degree to up),  250 45 degree to down)
@@ -72,7 +72,7 @@ void setup() {
 //
 //  pwm.setPWM(12, 0, 560); // 395 horizontal, 265 (45 to down), ~160 tallest, 560 shortest
 //  pwm.setPWM(13, 0, 230); // 160 outmost / leftmost, 230 vertical, 370 (45 to in), 460 innermost
-  pwm.setPWM(14, 0, 380); // 380 vertical, 250 (45 to front), 510 (45 to back), 150 towards front (might hit LF leg), 650 toward back
+//  pwm.setPWM(14, 0, 250); // 380 vertical, 250 (45 to front), 510 (45 to back), 150 towards front (might hit LF leg), 650 toward back
 //  
 }
 
