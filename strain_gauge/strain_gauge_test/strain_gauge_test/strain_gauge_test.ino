ab985aaffa17f459238e9b5b2a58c23d38ae0c1c
AdvisorChuanChuan <licha@shanghaitech.edu.cn>
2020-09-07 10:29:52 +0800

    [strain_gauge] add library and code

---
+++ strain_gauge/strain_gauge_test/strain_gauge_test/strain_gauge_test.ino
@@ -0,0 +1,14 @@
+#include "HX711.h"
+
+HX711 scale(2,3);
+long int values = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(scale.read());
+  delay(100);
+}
