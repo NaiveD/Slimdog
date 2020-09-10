6b5aa903d4289e55536a1c400e3236b05675986d
AdvisorChuanChuan <licha@shanghaitech.edu.cn>
2020-09-10 13:22:45 +0800

    [strain gauge] accept 2 readings from different cells

--- strain_gauge/strain_gauge_test/strain_gauge_test/strain_gauge_test.ino
+++ strain_gauge/strain_gauge_test/strain_gauge_test/strain_gauge_test.ino
@@ -1,14 +1,36 @@
 #include "HX711.h"
-// https://www.youtube.com/watch?v=cztWFrd-gQA
-HX711 scale(2,3);
-long int values = 0;
+
+// HX711 circuit wiring
+const int LOADCELL1_DOUT_PIN = 2;
+const int LOADCELL1_SCK_PIN = 3;
+const int LOADCELL2_DOUT_PIN = 5;
+const int LOADCELL2_SCK_PIN = 6;
+HX711 scale;
+HX711 scale2;
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(57600);
+  scale.begin(LOADCELL1_DOUT_PIN, LOADCELL1_SCK_PIN);
+  scale2.begin(LOADCELL2_DOUT_PIN, LOADCELL2_SCK_PIN);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(scale.read());
-  delay(100);
+
+  if (scale.is_ready()) {
+    long reading = scale.read();
+    Serial.print("Cell_1 reading: ");
+    Serial.println(reading);
+  } else {
+    Serial.println("Cell_1 not found.");
+  }
+  if (scale2.is_ready()) {
+    long reading2 = scale2.read();
+    Serial.print("Cell_2 reading: ");
+    Serial.println(reading2);
+  } else {
+    Serial.println("Cell_2 not found.");
+  }
+
+  delay(1000);
+  
 }
