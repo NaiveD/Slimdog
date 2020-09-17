9dd46743330e07912e15efc95021ef47115901ab
AdvisorChuanChuan <licha@shanghaitech.edu.cn>
2020-09-17 13:48:39 +0800

    [strain gauge] going to be calibrated

--- strain_gauge/strain_gauge_test/strain_gauge_test/strain_gauge_test.ino
+++ strain_gauge/strain_gauge_test/strain_gauge_test/strain_gauge_test.ino
@@ -13,13 +13,22 @@ void setup() {
   scale.begin(LOADCELL1_DOUT_PIN, LOADCELL1_SCK_PIN);
   scale2.begin(LOADCELL2_DOUT_PIN, LOADCELL2_SCK_PIN);
 }
+  int times = 0;
+  long sum1 = 0;
 
 void loop() {
-
+  if(times>10){
+    Serial.print("Cell 1 average: ");
+    Serial.print((double)sum1/(double)times);
+    delay(5000);
+    exit(-1);
+  }
   if (scale.is_ready()) {
     long reading = scale.read();
+    sum1 += reading;
     Serial.print("Cell_1 reading: ");
     Serial.println(reading);
+    times += 1;
   } else {
     Serial.println("Cell_1 not found.");
   }
@@ -30,7 +39,6 @@ void loop() {
   } else {
     Serial.println("Cell_2 not found.");
   }
-
   delay(1000);
   
 }
