882145f1d2ef01e747488c78060f1b870fb6e8da
AdvisorChuanChuan <licha@shanghaitech.edu.cn>
2020-09-18 18:33:58 +0800

    [strain_gauge] finish sample func and cali func, need to obtain a consistent hardware

--- strain_gauge/strain_gauge_test/strain_gauge_test/strain_gauge_test.ino
+++ strain_gauge/strain_gauge_test/strain_gauge_test/strain_gauge_test.ino
@@ -1,5 +1,4 @@
 #include "HX711.h"
-
 // HX711 circuit wiring
 const int LOADCELL1_DOUT_PIN = 2;
 const int LOADCELL1_SCK_PIN = 3;
@@ -7,38 +6,59 @@ const int LOADCELL2_DOUT_PIN = 5;
 const int LOADCELL2_SCK_PIN = 6;
 HX711 scale;
 HX711 scale2;
-
+unsigned int starting_time = 4000;
+unsigned int sample_start_time;
+const int max_sample_times = 20;
+long sample_count = 0;
+double list1[max_sample_times], list2[max_sample_times];
 void setup() {
   Serial.begin(57600);
   scale.begin(LOADCELL1_DOUT_PIN, LOADCELL1_SCK_PIN);
   scale2.begin(LOADCELL2_DOUT_PIN, LOADCELL2_SCK_PIN);
-}
-  int times = 0;
-  long sum1 = 0;
-
-void loop() {
-  if(times>10){
-    Serial.print("Cell 1 average: ");
-    Serial.print((double)sum1/(double)times);
-    delay(5000);
-    exit(-1);
-  }
-  if (scale.is_ready()) {
-    long reading = scale.read();
-    sum1 += reading;
-    Serial.print("Cell_1 reading: ");
-    Serial.println(reading);
-    times += 1;
-  } else {
-    Serial.println("Cell_1 not found.");
-  }
-  if (scale2.is_ready()) {
-    long reading2 = scale2.read();
-    Serial.print("Cell_2 reading: ");
-    Serial.println(reading2);
-  } else {
-    Serial.println("Cell_2 not found.");
-  }
+  scale.set_scale(297.082);
+  scale2.set_scale(381795.375);
+  Serial.println("- - - - - Start tare - - - - -");
+  scale.tare();
+  scale2.tare();
+  Serial.println("- - - - - End tare (activate now) - - - - -");
   delay(1000);
-  
+  Serial.println("- - - - - Counting down on start phase - - - - -");
+  delay(starting_time);
+  Serial.println("- - - - - Begin sampling - - - - -");
+}
+void loop() {
+  if(sample_count % 10 == 0){
+    Serial.print(sample_count);
+    Serial.println(" samples processed");
+  }
+  if(sample_count >= max_sample_times){
+    double sum1=0, sum2=0;
+    for(unsigned int i=0; i<max_sample_times; ++i){
+      sum1 += list1[i];
+      sum2 += list2[i];
+    }
+    Serial.print("Cell 1 result: \t");
+    Serial.println(sum1/max_sample_times);
+    Serial.print("Cell 2 result: \t");
+    Serial.println(sum2/max_sample_times);
+    delay(1500);
+    exit(-1); 
+  }
+  if(!scale.is_ready() || !scale2.is_ready()){
+    Serial.println("Disconnection appeared");
+    if(!scale.is_ready()){
+      Serial.print("Cell 1 disconnected at sample ");
+      Serial.println(sample_count);
+    }
+    if(!scale2.is_ready()){
+      Serial.print("Cell 2 disconnected at sample ");
+      Serial.println(sample_count);
+    }
+    delay(2000);
+    exit(-2);
+  }
+  list1[sample_count] = scale.get_units(5);
+  list2[sample_count] = scale2.get_units(5);
+  ++sample_count;
+  delay(100);
 }
